54b8816e20e100cec58f62e5dce9cb60fdd00192
nothiaki <celsohenrique367@gmail.com>
2025-07-14 21:31:27 -0300

    feat(trtl3-core): init spring boot

---
+++ trtl3-core/src/main/java/com/github/nothiaki/trtl3_core/Trtl3CoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.nothiaki.trtl3_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trtl3CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trtl3CoreApplication.class, args);
+	}
+
+}
---
+++ trtl3-core/src/test/java/com/github/nothiaki/trtl3_core/Trtl3CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nothiaki.trtl3_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trtl3CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
