f679dd2bc1faf748a907acf6ef1df9bcba7d1820
nothiaki <celsohenrique367@gmail.com>
2025-07-28 12:37:25 -0300

    feat(trtl3-golang-sdk): list created buckets

--- trtl3-golang-sdk/buckets.go
+++ trtl3-golang-sdk/buckets.go
@@ -1,6 +1,7 @@
 package trtl3sdk
 
 import (
+  "io"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -31,11 +32,51 @@ func (c *Client) CreateBucket(bucketName string) (bool, error) {
 	if err != nil {
 		return false, fmt.Errorf("Error while doing a request to the server: %w", err)
 	}
+
 	defer res.Body.Close()
 
-	if res.StatusCode == http.StatusCreated {
-		return true, nil
+	if res.StatusCode != http.StatusCreated {
+    return false, fmt.Errorf("Failed trying to create bucket(status: %d)", res.StatusCode)
 	}
 
-  return false, fmt.Errorf("Failed trying to create bucket(status: %d)", res.StatusCode)
+	return true, nil
+}
+
+func (c *Client) ListBuckets() ([]string, error) {
+	url := fmt.Sprintf("%s/buckets", c.url)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error trying to create the request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+  req.Header.Set("Authorization", c.token)
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error while doing a request to the server: %w", err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+    return nil, fmt.Errorf("Failed trying to find created buckets(status: %d)", res.StatusCode)
+	}
+
+ 	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading response body: %w", err)
+	}
+
+  buckets := []string{}
+
+  err = json.Unmarshal(bodyBytes, &buckets)
+  if err != nil {
+    return nil, fmt.Errorf("Error when deserialize response %w", err)
+  }
+
+  return buckets, nil
+
 }
