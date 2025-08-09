d5e6c932a94a6ee3867021b0a32e4d06593b1f7e
nothiaki <celsohenrique367@gmail.com>
2025-08-09 18:50:04 -0300

    chore(trtl3-web-docs): styles

--- trtl3-web-docs/src/components/card.tsx
+++ trtl3-web-docs/src/components/card.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function Card({ icon, title, desc }: Props) {
   return (
-    <div className="p-6 rounded-xl border border-gray-900">
+    <div className="p-6 rounded-xl border border-gray-900 text-gray-900">
       <IconBadge icon={icon} />
       <h3 className="text-xl font-semibold mb-2 mt-4">{title}</h3>
       <p className="text-gray-900">{desc}</p>
