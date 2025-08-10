62b52851dbaf040516da28a1fac6b6ba162c76f6
nothiaki <celsohenrique367@gmail.com>
2025-08-10 12:01:18 -0300

    feat(trtl3-node-sdk): delete buckets

--- trtl3-node-sdk/lib/buckets.ts
+++ trtl3-node-sdk/lib/buckets.ts
@@ -18,7 +18,7 @@ export class BucketApi {
     }
   }
 
- async list(): Promise<string[]> {
+  async list(): Promise<string[]> {
     try {
       const res = await this.client.get(`/buckets`);
 
@@ -29,6 +29,20 @@ export class BucketApi {
       return res.data;
     } catch (err: unknown) {
       return [];
+    }
+  }
+
+  async remove(bucketName: string): Promise<boolean> {
+    try {
+      const res = await this.client.delete(`/buckets?bucket=${bucketName}`);
+
+      if (res.status != 200) {
+        return false;
+      }
+
+      return true;
+    } catch (err: unknown) {
+      return false;
     }
   }
 
