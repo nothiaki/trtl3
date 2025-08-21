079ad9d49f45ad1c76abdfb83e15379f279cf368
nothiaki <celsohenrique367@gmail.com>
2025-08-21 15:00:42 -0300

    feat(examples): init trtl3

---
+++ examples/go-example/internal/storage/storage.go
@@ -0,0 +1,53 @@
+package storage
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	trtl3sdk "github.com/nothiaki/trtl3/trtl3-golang-sdk"
+)
+
+var trtl3 *trtl3sdk.Client
+
+func StorageInit() error {
+	trtl3 = trtl3sdk.Init(
+		"http://localhost:7713/",
+		"trtl3-token",
+	)
+
+	if _, err := trtl3.CreateBucket("cats"); err != nil {
+		fmt.Println("Error while creating bucket")
+		return err
+	}
+
+	for i := range 3 {
+		res, err := http.Get("https://cataas.com/cat")
+    if err != nil {
+			fmt.Println("Error fetching cat image:", err)
+			return err
+		}
+		defer res.Body.Close()
+
+		_, err = trtl3.UploadObject(
+			"cats",
+			res.Body,
+			fmt.Sprintf("%d.png", i),
+		)
+		if err != nil {
+			fmt.Println("Error uploading image:", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func FindAllCatImage() ([]string, error){
+	images, err := trtl3.ListObjects("cats")
+	if err != nil {
+		fmt.Println("Error uploading image:", err)
+	}
+
+	return images, nil
+}
